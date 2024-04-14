db21cf6d379c738d58c836d0df86711633804901
Yohan <yohan.brl@gmail.com>
2024-04-14 19:43:08 +0200

    chores: add reverse proxy for posthog

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/ingest/static/:path*',
+        destination: 'https://eu-assets.i.posthog.com/static/:path*',
+      },
+      {
+        source: '/ingest/:path*',
+        destination: 'https://eu.i.posthog.com/:path*',
+      },
+    ];
+  },
+};
 
 export default nextConfig;
--- src/lib/providers.js
+++ src/lib/providers.js
@@ -5,9 +5,11 @@ import { ReactNode } from 'react';
 
 if (typeof window !== 'undefined') {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    api_host: 'https://yohanlebreton.com/ingest',
+    ui_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
   });
 }
+
 export function CSPostHogProvider({ children }) {
   return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
 }
