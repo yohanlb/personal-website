dec073afe1443d66472999a895d447b4b19a3bba
Yohan <yohan.brl@gmail.com>
2024-04-15 14:00:36 +0200

    chores: disable posthog on dev env

--- src/lib/providers.js
+++ src/lib/providers.js
@@ -3,7 +3,10 @@ import posthog from 'posthog-js';
 import { PostHogProvider } from 'posthog-js/react';
 import { ReactNode } from 'react';
 
-if (typeof window !== 'undefined') {
+const isEnabled =
+  typeof window !== 'undefined' && process.env.NODE_ENV !== 'development';
+
+if (isEnabled) {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
     api_host: 'https://yohanlebreton.com/ingest',
     ui_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
@@ -11,5 +14,9 @@ if (typeof window !== 'undefined') {
 }
 
 export function CSPostHogProvider({ children }) {
-  return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
+  if (isEnabled) {
+    return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
+  } else {
+    return <>{children}</>;
+  }
 }
