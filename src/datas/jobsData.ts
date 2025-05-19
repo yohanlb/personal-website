ccd7b78785918f918e5509cf0913b4d552b212c4
yohanlb <yohan.lbtn@proton.me>
2025-05-19 15:17:41 +0200

    feat: add freenow to work section

--- src/datas/jobsData.ts
+++ src/datas/jobsData.ts
@@ -8,26 +8,32 @@ export type JobData = {
 export const workData: JobData[] = [
   {
     title: 'Web Engineer',
-    company: 'Frichti',
-    years: '2021-2024',
-    imgUrl: '/images/work/frichti.jpeg',
+    company: 'Freenow',
+    years: '2025 - Now',
+    imgUrl: '/images/work/freenow.jpeg',
   },
   {
     title: 'Web Engineer',
-    company: 'Gorillas',
-    years: '2022-2023',
+    company: 'Gorillas (Acquired Frichti)',
+    years: '2022 - 2024',
     imgUrl: '/images/work/gorillas.jpeg',
+  },
+  {
+    title: 'Web Engineer',
+    company: 'Frichti',
+    years: '2021 - 2024',
+    imgUrl: '/images/work/frichti.jpeg',
   },
   {
     title: 'Full Stack Engineer',
     company: 'Novelab',
-    years: '2020-2021',
+    years: '2020 - 2021',
     imgUrl: '/images/work/novelab.jpeg',
   },
   {
     title: 'Full Stack Engineer / Creative Technologist',
     company: 'AC3 Studio / VR-Studio',
-    years: '2017-2020',
+    years: '2017 - 2020',
     imgUrl: '/images/work/ac3.png',
   },
 ];
